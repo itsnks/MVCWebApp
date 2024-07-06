982bf9d7d3b7f762fbc6c1588c5daee2b99e5897
Nikesh <subedi.nikesh@outlook.com>
2024-07-06 14:07:05 +0545

    defined model for category table

--- MVCWebApp/Models/Category.cs
+++ MVCWebApp/Models/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace MVCWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCWebApp.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
     }
 }
