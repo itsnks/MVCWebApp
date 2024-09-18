4cd7c84b6051ae8d4800daab8bd0b0f6bab7304f
Nikesh <subedi.nikesh@outlook.com>
2024-09-18 22:13:17 +0545

    configured controller to retreive DB objects as Lists

--- MVCWebApp/Controllers/CategoryController.cs
+++ MVCWebApp/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCWebApp.Data;
+using MVCWebApp.Models;
 
 namespace MVCWebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDBContext _db;
+        public CategoryController(AppDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
