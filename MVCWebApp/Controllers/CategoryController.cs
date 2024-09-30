01bacb30a5fd717df49bfe910fb2202e51a46aa4
Nikesh <subedi.nikesh@outlook.com>
2024-09-30 21:50:36 +0545

    edit and delete options

--- MVCWebApp/Controllers/CategoryController.cs
+++ MVCWebApp/Controllers/CategoryController.cs
@@ -36,5 +36,63 @@ namespace MVCWebApp.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.Find(id);
+            /*Category? categoryFromDB1 = _db.Categories.FirstOrDefault(u => u.Id == id);
+            Category? categoryFromDB2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();*/
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The display order can't be the same as the Category Name!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDB = _db.Categories.Find(id);
+
+            if (categoryFromDB == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDB);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
