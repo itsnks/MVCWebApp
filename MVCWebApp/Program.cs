9c1d5739c0782e26dd7b723799bbb36b8d73f7be
Nikesh <subedi.nikesh@outlook.com>
2025-06-26 21:23:27 +0545

    configured for postgres

--- MVCWebApp/Program.cs
+++ MVCWebApp/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDBContext>(options => 
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
